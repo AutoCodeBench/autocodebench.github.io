7464a5a2ed133faf9e3dcbe24b7ccb0426bcd131
Jason <zhouchangzhi97@gmail.com>
2025-08-11 21:41:14 +0800

    delete

--- back/scripts/rename_html.sh
+++
@@ -1,5 +0,0 @@
-# Rename build/index.html to build/leaderboard.html
-# Rename build/index_home.thml to build/index.html
-
-mv build/index.html build/leaderboard.html
-mv build/index_home.html build/index.html
